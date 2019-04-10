57ab66748afaac866d5c0f13fb2b5a3f1cb4fae8
Sebastian Kurscheid <genomicsio@gmail.com>
2019-04-10 14:56:39 +1000

    makes minor modifications

--- scripts/compute_matrix_commands.sh
+++ scripts/compute_matrix_commands.sh
@@ -1,25 +1,32 @@
-export bwG1Dir="/data/experiment/ChIP-Seq/LR1807201/N08851_SK_LR1807201_SEQ/deepTools/bamCompare/GRCh38_ensembl84/G1"
-export bwMDir="/data/experiment/ChIP-Seq/LR1807201/N08851_SK_LR1807201_SEQ/deepTools/bamCompare/GRCh38_ensembl84/M"
-export idrG1Dir="/data/experiment/ChIP-Seq/LR1807201/N08851_SK_LR1807201_SEQ/idr/BEDs/GRCh38_ensembl84/G1"
-export idrMDir="/data/experiment/ChIP-Seq/LR1807201/N08851_SK_LR1807201_SEQ/idr/BEDs/GRCh38_ensembl84/M"
-export matrixOutM="/data/experiment/ChIP-Seq/LR1807201/N08851_SK_LR1807201_SEQ/deepTools/computeMatrix/GRCh38_ensembl84/M/combined"
-export matrixOutG1="/data/experiment/ChIP-Seq/LR1807201/N08851_SK_LR1807201_SEQ/deepTools/computeMatrix/GRCh38_ensembl84/G1/combined"
+export baseDir="/home/sebastian/Data/Tremethick/CellCycle"
+export bwG1Dir="${baseDir}/ChIP-Seq/LR1807201/N08851_SK_LR1807201_SEQ/deepTools/bamCompare/log2/GRCh38_ensembl84/G1"
+export bwMDir="${baseDir}/ChIP-Seq/LR1807201/N08851_SK_LR1807201_SEQ/deepTools/bamCompare/log2/GRCh38_ensembl84/M"
+export idrG1Dir="${baseDir}/ChIP-Seq/LR1807201/N08851_SK_LR1807201_SEQ/idr/callpeak_input_only/BEDs/GRCh38_ensembl84/G1"
+export idrMDir="${baseDir}/ChIP-Seq/LR1807201/N08851_SK_LR1807201_SEQ/idr/callpeak_input_only/BEDs/GRCh38_ensembl84/M"
+export matrixOut="${baseDir}/ChIP-Seq/LR1807201/N08851_SK_LR1807201_SEQ/deepTools/computeMatrix/GRCh38_ensembl84"
+
+export matrixOutM="${baseDir}/ChIP-Seq/LR1807201/N08851_SK_LR1807201_SEQ/deepTools/computeMatrix/GRCh38_ensembl84/M/combined"
+export matrixOutG1="${baseDir}/ChIP-Seq/LR1807201/N08851_SK_LR1807201_SEQ/deepTools/computeMatrix/GRCh38_ensembl84/G1/combined"
+
+export pdfOut="${baseDir}/ChIP-Seq/LR1807201/N08851_SK_LR1807201_SEQ/deepTools/plotHeatmap/"
 
 export beforeRegionStartLength="1000"
 export afterRegionStartLength="1000"
-export threads="8"
+export threads="32"
 
-computeMatrix reference-point --scoreFileName $(ls $bwMDir/*.bw|tr "\n" " ") \
-                              --regionsFileName $(ls $idrMDir/*/*idr.bed|tr "\n" " ")\
-                              --outFileName $matrixOutM/matrix.gz\
-                              --outFileSortedRegions $matrixOutM/matrix.bed\
+# plotting coverage of different libraries across H2A.Z peaks
+computeMatrix reference-point --scoreFileName $(ls $bwG1Dir/*.bw|tr "\n" " "; ls $bwMDir/*.bw|tr "\n" " ") \
+                              --regionsFileName $idrG1Dir/H2AZG1/H2AZG1_idr.sorted.bed $idrMDir/H2AZM/H2AZM_idr.sorted.bed \
+                              --outFileName $matrixOut/matrix_H2AZ_peaks_log2.gz\
+                              --outFileSortedRegions $matrixOut/matrix_H2AZ_peaks_log2.bed\
+                              --numberOfProcessors 32\
                               --referencePoint center\
                               --beforeRegionStartLength $beforeRegionStartLength\
                               --afterRegionStartLength $afterRegionStartLength\
-                              --numberOfProcessors $threads
+                              
 
-plotHeatmap --matrixFile $matrixOutM/matrix.gz\
-            --outFileName heatmap.pdf\
+plotHeatmap --matrixFile $matrixOut/matrix_H2AZ_peaks_log2.gz\
+            --outFileName $pdfOut/heatmap_H2AZ_peaks_log2.pdf\
             --averageTypeSummaryPlot mean\
             --refPointLabel "Peak center"\
 
